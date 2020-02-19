8d91d22525b470329ed591a90f90fdb9a728b0ca
akane fukui <akane.1418@gmail.com>
2020-02-19 16:58:00 +0000

    yelp MVP

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,28 +3,26 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def create
-
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
-
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
-  def edit
-
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def show
+  private
 
-  end
-
-  def update
-
-  end
-
-  def destroy
-
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :restaurants do
- end
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new'
+  # get '/restaurants/:id', to: 'restaurants#show', as: :show
+  # post '/restaurants', to: 'restaurants#create', as: :restaurants_create
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: :restaurants_edit
+  # patch '/restaurants/:id', to: 'restaurants#update', as: :restaurants_update
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
 
-end
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    end
+  end
+
